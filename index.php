46f7e6a92acf7adb85c9543b40a4d955c4d09fd2
Fernando Campos <fhcs2630@gmail.com>
2018-08-14 15:14:08 -0300

    usando materialize e construindo banco de dados

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="materialize/css/materialize.min.css">
+    <title></title>
+</head>
+<body>
+
+
+<nav class="blue-grey">
+    <div class="nav-wrapper container">
+
+        <div class="brand-logo light">Sistema de cadastro</div>
+        <ul class="right">
+            <li><a href="http://"><i class="material-icons left">account_circle</i>Cadastro</a></li>
+            <li><a href="http://"><i class="material-icons left">search</i>Consultas</a></li>
+          
+        </ul>
+        </div>
+</nav>
+
+
+
+
+<script type="text/javascript" src="materialize/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+});
+</script>
+
+</body>
+</html>
