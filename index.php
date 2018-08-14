a5f61dfc3acf72853b7349e76d637a926b6fd550
Fernando Campos <fhcs2630@gmail.com>
2018-08-14 16:01:56 -0300

    formulario com HTML E o Materialize

--- index.php
+++ index.php
@@ -26,7 +26,31 @@
     <p>&nbsp;</p>
 <form action="data/create.php" method="post" class="col s12">
     <fieldset class="formulario">
-        <legend>Cadastro</legend>
+        <legend><img src="img/Small-mario.png" alt="[img]" width="100"></legend>
+        <h5 class="light center ">Cadastro de clientes</h5>
+
+        <div class="inpu field col s12">
+            <i class="material-icons prefix">account_circle</i>
+            <input type="text" name="name" id="name" maxlength="40" required autofocus>
+            <label for="name">Nome do cliente </label>
+        </div>
+
+        <div class="inpu field col s12">
+            <i class="material-icons prefix">email</i>
+            <input type="email" name="email" id="email" maxlength="50" required >
+            <label for="email">email do cliente </label>
+        </div>
+
+        <div class="inpu field col s12">
+            <i class="material-icons prefix">phone</i>
+            <input type="tel" name="phone" id="phone" maxlength="15" required >
+            <label for="phone">telefone do cliente </label>
+        </div>
+
+        <div class="input field col s12">
+            <input type="submit" value="cadastrar" class="btn blue">
+            <input type="reset" value="limpar" class="btn red">
+        </div>
     </fieldset>
 
 
